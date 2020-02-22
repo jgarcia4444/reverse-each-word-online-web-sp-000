087c2c3509158f783136444fb40b1f0ecd7954a8
Jake Garcia <bballwiz4@gmail.com>
2020-02-22 17:18:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   sent_arr = sentence.split(" ")
   sent_arr.collect do |word|
-    word.reverse
+    word.split("").reverse.join("")
   end
   sent_arr.join(" ")
 end
